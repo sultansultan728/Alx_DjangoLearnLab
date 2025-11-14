2d024f03e1f4a0c7f009c3d9ce12ed5ddb033916
sultansultan728 <sultansultan7659999@gmail.com>
2025-11-14 07:48:48 -0800

    0. Implementing a Custom User Model in Django

---
+++ LibraryProject/LibraryProject/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
--- LibraryProject/LibraryProject/settings.py
+++ LibraryProject/LibraryProject/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LibraryProject/LibraryProject/settings.py
+AUTH_USER_MODEL = 'users.CustomUser'
+
