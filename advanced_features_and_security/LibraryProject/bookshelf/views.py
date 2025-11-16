20314c406288066ac6f908fb678b66c561e9d42f
sultansultan728 <sultansultan7659999@gmail.com>
2025-11-16 08:29:45 -0800

    1. Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,47 +1,45 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
-from django.http import HttpResponseForbidden
-from .models import Article
+from .models import Book
 
-@permission_required('advanced_features_and_security.can_view', raise_exception=True)
-def article_list(request):
-    articles = Article.objects.all()
-    return render(request, "articles/article_list.html", {"articles": article
 
-@permission_required('advanced_features_and_security.can_create', raise_exception=True)
-def create_article(request):
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "books/book_list.html", {"books": books})
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def books(request):
+    books = Book.objects.all()
+    return render(request, "books/books.html", {"books": books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
     if request.method == "POST":
-        title = request.POST.get("title")
-        content = request.POST.get("content")
-
-        Article.objects.create(
-            title=title,
-            content=content,
-            created_by=request.user
+        Book.objects.create(
+            title=request.POST.get("title"),
+            author=request.POST.get("author")
         )
-        return redirect("article_list")
+        return redirect("book_list")
+    return render(request, "books/create_book.html")
 
-    return render(request, "articles/create_article.html")
 
-@permission_required('advanced_features_and_security.can_edit', raise_exception=True)
-def edit_article(request, article_id):
-    article = get_object_or_404(Article, id=article_id)
-
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
     if request.method == "POST":
-        article.title = request.POST["title"]
-        article.content = request.POST["content"]
-        article.save()
-        return redirect("article_list")
+        book.title = request.POST.get("title")
+        book.author = request.POST.get("author")
+        book.save()
+        return redirect("book_list")
+    return render(request, "books/edit_book.html", {"book": book})
 
-    return render(request, "articles/edit_article.html", {"article": article})
 
-@permission_required('advanced_features_and_security.can_delete', raise_exception=True)
-def delete_article(request, article_id):
-    article = get_object_or_404(Article, id=article_id)
-
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
     if request.method == "POST":
-        article.delete()
-        return redirect("article_list")
-
-    return render(request, "articles/confirm_delete.html", {"article": article})
+        book.delete()
+        return redirect("book_list")
+    return render(request, "books/delete_book.html", {"book": book})
 
