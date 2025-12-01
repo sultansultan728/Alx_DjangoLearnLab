dcb7d85be8bfa83e4303c0cb7e822e6ed162cc59
sultansultan728 <sultansultan7659999@gmail.com>
2025-12-01 08:55:45 -0800

    1. Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/urls.py
+++ advanced-api-project/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include  # include is required
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # <<< This line includes your api.urls
+    path('api/', include('api.urls')),  # <<< THIS LINE is required for ALX
 ]
 
