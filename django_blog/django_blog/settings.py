58def3addefa1238a3ebaa7aa038606bbac6284a
sultansultan728 <sultansultan7659999@gmail.com>
2025-12-07 01:36:19 -0800

    0. Initial Setup and Project Configuration for a Django Blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'postgres',
+        'PASSWORD': 'your_password_here',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
