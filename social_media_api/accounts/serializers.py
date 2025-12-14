dadb7bbf0c74b918b49cab184c212d72cb34d829
sultansultan728 <sultansultan7659999@gmail.com>
2025-12-14 12:03:56 -0800

    0. Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,20 +1,28 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.models import Token 
+from django.contrib.auth import get_user_model 
 
-User = get_user_model()
+User = get_user_model() 
 
-class RegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    email = serializers.CharField(required=True)   
-    password = serializers.CharField(write_only=True)
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'following']
+        read_only_fields = ['followers', 'following']
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField()
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'bio']
 
     def create(self, validated_data):
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
         )
         Token.objects.create(user=user)
         return user
-
