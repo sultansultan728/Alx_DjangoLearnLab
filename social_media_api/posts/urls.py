7ea741ea2b0b72f260823ee93c1583df1acb9547
sultansultan728 <sultansultan7659999@gmail.com>
2025-12-15 04:59:13 -0800

    2. Implementing User Follows and Feed Functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,9 +1,14 @@
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
+from django.urls import path
+from .views import feed
+
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
 urlpatterns = router.urls
-
+urlpatterns = [
+    path('feed/', feed),
+]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,7 +3,22 @@ from rest_framework import viewsets, permissions, filters
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def feed(request):
+    following_users = request.user.following.all()
+    posts = Post.objects.filter(
+        author__in=following_users
+    ).order_by('-created_at')
+
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
+
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
