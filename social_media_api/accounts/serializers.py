e4943858b893976752256ff4666436035128d229
sultansultan728 <sultansultan7659999@gmail.com>
2025-12-15 04:23:32 -0800

    1. Implementing Posts and Comments Functionality

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,28 +1,38 @@
+from .models import CustomUser
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token 
-from django.contrib.auth import get_user_model 
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
-User = get_user_model() 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'following']
-        read_only_fields = ['followers', 'following']
-
-class UserRegistrationSerializer(serializers.ModelSerializer):
+class CustomUserSerializer(serializers.ModelSerializer):
+    
     password = serializers.CharField()
     
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password', 'bio']
+        model = CustomUser
+        fields = ['username', 'email', 'bio', 'profile_picture']
 
+    
     def create(self, validated_data):
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            bio=validated_data.get('bio', ''),
-        )
+            email=validated_data['email'], 
+            bio=validated_data['bio'], 
+            profile_picture=validated_data['profile_picture'], **validated_data)
+        
+    
         Token.objects.create(user=user)
+        return user
+    
+    
+class LoginUserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = get_user_model().objects.filter(username=data['username']).first()
+        if user is None:
+            raise serializers.ValidationError('User does not exist')
+        if not user.check_password(data['password']):
+            raise serializers.ValidationError('Incorrect password')
         return user
